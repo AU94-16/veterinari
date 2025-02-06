cf79cbed3356c6905d5fc4ed76c74f8dead0f5ed
Carmen-Della-Monica <carmen95dm@gmail.com>
2025-02-06 15:48:34 +0100

    Model class

--- src/main/java/com/example/veterinari/dao/ProprietarioDao.java
+++ src/main/java/com/example/veterinari/dao/ProprietarioDao.java
@@ -1,5 +1,6 @@
 package com.example.veterinari.dao;
 
+import com.example.veterinari.model.Proprietario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProprietarioDao extends CrudRepository<Proprietario, Integer> {
--- src/main/java/com/example/veterinari/dao/StoricoDao.java
+++ src/main/java/com/example/veterinari/dao/StoricoDao.java
@@ -1,5 +1,6 @@
 package com.example.veterinari.dao;
 
+import com.example.veterinari.model.Storico;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StoricoDao extends CrudRepository<Storico, Integer> {
