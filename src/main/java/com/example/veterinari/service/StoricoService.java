23be8a5199492c9e3921880800ea712a6fa4410e
AU94-16 <annamaria.u16@gmail.com>
2025-02-06 17:36:18 +0100

    Storico service Annamaria

--- src/main/java/com/example/veterinari/service/StoricoService.java
+++ src/main/java/com/example/veterinari/service/StoricoService.java
@@ -2,12 +2,16 @@ package com.example.veterinari.service;
 
 import com.example.veterinari.model.Storico;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+
 
 public interface StoricoService {
 
     List<Storico> elencoStorico();
+    Storico datiStorico(int id);
+    boolean aggiuntaStorico(Storico storico, int idAnimale, String tipo, String nome, String dosaggio_dettaglio, LocalDateTime data_ora_prestazione, LocalDateTime data_ora_richiamo, String note);
+    boolean eliminazioneStorico(int id);
 
     }
 
