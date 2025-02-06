157d021320eda8ceffd975e08a031a14929dc23d
AU94-16 <annamaria.u16@gmail.com>
2025-02-06 16:42:53 +0100

    Creazione storico service Annamaria

---
+++ src/main/java/com/example/veterinari/service/StoricoService.java
@@ -0,0 +1,14 @@
+package com.example.veterinari.service;
+
+import com.example.veterinari.model.Storico;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoricoService {
+
+    List<Storico> elencoStorico();
+
+    }
+
+
